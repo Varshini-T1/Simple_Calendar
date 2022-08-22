778790ae001eb7a08fdb3939f4050d5011ed942b
Varshini-T1 <111520536+Varshini-T1@users.noreply.github.com>
2022-08-22 14:47:07 +0530

    Update simpleCalendar.c

--- simpleCalendar.c
+++ simpleCalendar.c
@@ -1,77 +1,63 @@
 #include<stdio.h>
-#define TRUE    1
-#define FALSE   0
 
 int days_in_month[]={0,31,28,31,30,31,30,31,31,30,31,30,31};
 
 char *months[]=
 {
 	" ",
-	"\n\n\nJanuary",
-	"\n\n\nFebruary",
-	"\n\n\nMarch",
-	"\n\n\nApril",
-	"\n\n\nMay",
-	"\n\n\nJune",
-	"\n\n\nJuly",
-	"\n\n\nAugust",
-	"\n\n\nSeptember",
-	"\n\n\nOctober",
-	"\n\n\nNovember",
-	"\n\n\nDecember"
+	"\n\nJanuary",
+	"\n\nFebruary",
+	"\n\nMarch",
+	"\n\nApril",
+	"\n\nMay",
+	"\n\nJune",
+	"\n\nJuly",
+	"\n\nAugust",
+	"\n\nSeptember",
+	"\n\nOctober",
+	"\n\nNovember",
+	"\n\nDecember"
 };
-
-int inputyear(void)
-{
-	int year;
-	printf("Please enter a year (example: 2000) : ");
-	scanf("%d", &year);
-	return year;
-}
 
 int determinedaycode(int year)
 {
-	int daycode;
-	int d1, d2, d3;
-	d1 = (year - 1.)/ 4.0;
-	d2 = (year - 1.)/ 100.;
-	d3 = (year - 1.)/ 400.;
+	int daycode,d1, d2, d3;
+	d1 = (year - 1)/ 4;
+	d2 = (year - 1)/ 100;
+	d3 = (year - 1)/ 400;
 	daycode = (year + d1 - d2 + d3) %7;
 	return daycode;
 }
 
-int determineleapyear(int year)
+void determineleapyear(int year)
 {
-	if(year% 4 == FALSE && year%100 != FALSE || year%400 == FALSE)
+	if(year% 4 == 0 && year%100 != 0 || year%400 == 0)
 	{
 		days_in_month[2] = 29;
-		return TRUE;
 	}
 	else
 	{
 		days_in_month[2] = 28;
-		return FALSE;
 	}
 }
 
 void calendar(int year, int daycode)
 {
-	int month, day;
-	for ( month = 1; month <= 12; month++ )
+	for (int month = 1; month <= 12; month++ )
 	{
 		printf("%s", months[month]);
 		printf("\n\nSun  Mon  Tue  Wed  Thu  Fri  Sat\n" );
 
-		for ( day = 1; day <= 1 + daycode * 5; day++ )
+		for (int space = 0; space < 1 + daycode * 5; space++ )
 		{
 			printf(" ");
 		}
 
-		for ( day = 1; day <= days_in_month[month]; day++ )
+		for (int date = 1; date <= days_in_month[month]; date++ )
 		{
-			printf("%2d", day );
+			printf("%2d", date );
 
-			if ( ( day + daycode ) % 7 > 0 )
+			if ( ( date + daycode ) % 7 > 0 )
 				printf("   " );
 			else
 				printf("\n " );
@@ -81,12 +67,12 @@ void calendar(int year, int daycode)
 	}
 }
 
-int main(void)
+void main()
 {
-	int year, daycode, leapyear;
-	year = inputyear();
+	int year,daycode;
+	printf("Please enter a year (example: 2000) : ");
+	scanf("%d", &year);
 	daycode = determinedaycode(year);
 	determineleapyear(year);
 	calendar(year, daycode);
-	printf("\n");
 }
